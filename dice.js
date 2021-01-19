32292c5467a2ce37121549c9ac775d1acb676d95
Heidi Hood <heidih74@gmail.com>
2021-01-19 18:36:43 -0500

    final-code

--- dice.js
+++ dice.js
@@ -1,68 +1,54 @@
-// let continueGame = true;
+//advanced (optional)
+let numberOfSides = prompt("How many sides does your dice have?");
+let numberOfRolls = prompt("How many times would you like to roll your dice?");
 
+let playingGame = true;
+while (playingGame) {
+  let rolls = 0;
+  while (rolls < numberOfRolls) {
+    let dice1 = Math.floor(Math.random() * numberOfSides + 1);
+    let dice2 = Math.floor(Math.random() * numberOfSides + 1);
+
+    console.log(`You rolled a ${dice1} and a ${dice2}`);
+    console.log(`Total: ${dice1 + dice2}`);
+
+    rolls++;
+  }
+  let input = prompt("Roll again? (y/n)");
+  if (input === "n") {
+    playingGame = false;
+  } 
+}
+
+//while loop
+// let playingGame = true;
+// while (playingGame) {
+//   let dice1 = Math.floor(Math.random() * 6 + 1);
+//   let dice2 = Math.floor(Math.random() * 6 + 1);
+
+//   console.log(`You rolled a ${dice1} and a ${dice2}`);
+//   console.log(`Total: ${dice1 + dice2}`);
+
+//   let input = prompt("Press 'Y' to roll again. Press 'S' to stop.");
+//   if (input === "S") {
+//     playingGame = false;
+//   }
+// }
+
+// do while loop
+// let playingGame = true
 // do {
-//   let diceOne = Math.floor(Math.random() * 6 + 1);
-//   let diceTwo = Math.floor(Math.random() * 6 + 1);
+//   // let dice1 = Math.floor((Math.random() * 6) + 1)
+//   // let dice2 = Math.floor((Math.random() * 6) + 1)
 
-//   console.log(`You rolled a ${diceOne} and a ${diceTwo}`);
-//   console.log(`Total: ${diceOne + diceTwo}`);
+//   console.log(`You rolled a ${dice1} and a ${dice2}`)
+//   console.log(`Total: ${dice1 + dice2}`)
 
-//   let input = prompt("Press 'Y' to roll again, 'S' to stop.");
+//   let input = prompt("Press 'Y' to roll again, press 'S' to stop")
 //   while (input !== "Y" && input !== "S") {
-//     input = prompt("Please enter 'Y' or 'S'.");
+//     input = prompt("Please enter 'Y' or 'S'.")
 //   }
 //   if (input === "S") {
-//     continueGame = false;
+//     playingGame = false
 //   }
-// } while (continueGame === true);
-
-//advanced
-// let numOfSides = prompt("How many sides does your dice have?");
-// let numOfRolls = prompt(
-//   "How many times would you like to roll your pair of dice?"
-// );
-
-// let continueGame = true;
-
-// while (continueGame) {
-//   let rolls = 0;
-//   while (rolls < numOfRolls) {
-//     let diceOne = Math.floor(Math.random() * numOfSides + 1);
-//     let diceTwo = Math.floor(Math.random() * numOfSides + 1);
-
-//     console.log(`You rolled a ${diceOne} and a ${diceTwo}`);
-//     console.log(`Total: ${diceOne + diceTwo}`);
-
-//     rolls++;
-//     continueGame = false;
-//   }
-//   let input = prompt("Roll Again? (y/n)");
-//   if (input === "y") {
-//     continueGame = true;
-//   } else {
-//     alert("Thanks for playing!");
-//   }
-// }
-
-//advanced using a for loop
-// let numOfSides = prompt("How many sides does your dice have?");
-// let numOfRolls = prompt(
-//   "How many times would you like to roll your pair of dice?"
-// );
-
-// let continueGame = true;
-
-// while (continueGame) {
-//   for (let i = 0; i < numOfRolls; i++) {
-//     let diceOne = Math.floor(Math.random() * numOfSides + 1);
-//     let diceTwo = Math.floor(Math.random() * numOfSides + 1);
-
-//     console.log(`You rolled a ${diceOne} and a ${diceTwo}`);
-//     console.log(`Total: ${diceOne + diceTwo}`);
-//   }
-
-//   let input = prompt("Roll again? (y/n)");
-//   if (input === "n") {
-//     continueGame = false;
-//   }
-// }
+// } while (playingGame)
